b403f48e085adfcb1c56d43fe2f0beb8797e72de
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-14 22:31:43 +0600

    And or

---
+++ main.py
@@ -0,0 +1,20 @@
+#Activity_1
+a = 2145
+b = 456
+c = -456
+d = 0
+e = 67
+
+if a>0 and b>0 and c>0 and d>0 and e>0:
+    print("All the numbers are greater than 0.")
+else:
+    print("Atleast one of the numbers are NOT greater than 0.")
+
+#Activity_2
+j = 90
+k = 0
+
+if j>0 or k>0:
+    print("Either or both of the number is greater than 0.")
+else:
+    print("No number is greater than 0.")
